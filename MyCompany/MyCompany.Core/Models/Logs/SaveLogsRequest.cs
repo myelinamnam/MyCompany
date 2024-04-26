310bbf28418cc641950b36c64d2828d99b599cb3
myelinamnam <michellegalangera@gmail.com>
2024-04-26 23:06:15 +0800

    re-pushed Logs table

---
+++ MyCompany/MyCompany.Core/Models/Logs/SaveLogsRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace MyCompany.Core.Models
+{
+    /// <summary>
+    /// Represents a request object for saving logs.
+    /// </summary>
+    public class SaveLogsRequest
+    {
+        /// <summary>
+        /// Gets or sets the endpoint associated with the request.
+        /// </summary>
+        public string Endpoint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request data.
+        /// </summary>
+        public string Request { get; set; }
+
+        /// <summary>
+        /// Gets or sets the response data.
+        /// </summary>
+        public string Response { get; set; }
+    }
+
+}
